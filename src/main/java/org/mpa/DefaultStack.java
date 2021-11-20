f4ed9c74dfcd5603a4cc36d55b158246bb8b3289
Franck Silvestre <franck.silvestre@gmail.com>
2021-11-20 11:11:10 +0100

    fix #2.1 projet compile

--- src/main/java/org/mpa/DefaultStack.java
+++ src/main/java/org/mpa/DefaultStack.java
@@ -1,4 +1,28 @@
 package org.mpa;
 
 public class DefaultStack implements SimpleStack {
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void push(Item item) {
+
+    }
+
+    @Override
+    public Item peek() throws EmptyStackException {
+        return null;
+    }
+
+    @Override
+    public Item pop() throws EmptyStackException {
+        return null;
+    }
 }
